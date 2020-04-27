9385944eade34789de1cb6b900e49903a959b8bf
MaskedFox <maskedfox@avavpn-170.vrt.sourcefire.com>
2020-04-27 17:31:25 -0400

    uploading third four exercises from learn Python 3 the Hard Way

--- learnPython3/ex11.py
+++ learnPython3/ex11.py
@@ -6,3 +6,11 @@ print("How much do you weigh?", end=" ")
 weight = input()
 
 print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+# The input() method reads a line from input, converts into a string and returns it.
+# The input() method reads a line from input (usually user), converts the line into a string by removing the trailing newline, and returns it.
+
+# If EOF is read, it raises an EOFError exception.
+
+age = input("How old are you?")
+print(age)
