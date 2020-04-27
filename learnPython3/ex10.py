11c8ea6686516b992721a2746c5fc239876485b3
MaskedFox <maskedfox@avavpn-170.vrt.sourcefire.com>
2020-04-27 17:10:19 -0400

    uploading third four exercises(3) from learn Python 3 the Hard Way

---
+++ learnPython3/ex10.py
@@ -0,0 +1,20 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = '''
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+''' #typo again forgot the \
+
+print("\t", tabby_cat)
+print(persian_cat, "\n")
+print("\n", backslash_cat)
+print(fat_cat)
+
+
+#TODO: Escape Sequence \r CR Carriage return? Need to google it
+# print('Python is included in CodeSpeedy\r123456')
+# 123456 is included in CodeSpeedy
---
+++ learnPython3/ex7.py
@@ -0,0 +1,27 @@
+# prints a string
+print("Mary had a little lamb.") #made a typo here before hda
+# Prints a string using format .format()
+print("Its fleece was white as {}.".format("snow"))
+print("And everywhere that Mary went.")
+# prints . 10 times
+print("." * 10) # what'd that do?
+# Sets each variable with one character from the two words Cheese Burger
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = 'r'
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch end = '' at the end. try removing it to see what happens
+# Prints the first 6 variables that makes Cheese and uses End=" " to print them together with an space in between
+print(end1 + end2 + end3 + end4 + end5 + end6, end=" ")
+print(end7 + end8 + end9 + end10 + end11 + end12)
+
+
