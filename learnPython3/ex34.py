f171e04e1c29d7dd654b82cc4d49edac89e19ebe
MaskedFox <maskedfox@MaskedFoxs-MacBook-Pro.local>
2020-05-10 23:06:05 -0400

    uploading 11th 4 exercises from learn Python 3 The Hard Way

--- learnPython3/ex34.py
+++ learnPython3/ex34.py
@@ -12,3 +12,14 @@ whale
 """
 
 print("\n", animals[1]," \n",animals[2],"\n", animals[0],"\n", animals[3],"\n", animals[4],"\n", animals[2],"\n", animals[5],"\n", animals[4],"\n")
+
+print(f"""
+{animals[1]}
+{animals[2]}
+{animals[0]}
+{animals[3]}
+{animals[4]}
+{animals[2]}
+{animals[5]}
+{animals[4]}
+""")
