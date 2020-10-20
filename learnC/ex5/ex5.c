ddfaf66901a9bbeb41d13e8988b62fa3868018e6
Masked Fox <iammaskedfx@gmail.com>
2020-10-20 18:53:07 -0400

    Completed ex 5

---
+++ learnC/ex5/ex5.c
@@ -0,0 +1,32 @@
+//TODO Watch the video
+
+++ // Increment
+
+-- // Decrement
+
+//TODO Read more about it
+Logical Operators:
+&& // Logical and
+|| // Logical or
+! // Logical not
+?: // Logical ternary
+
+
+Bitwise Operators:
+& // Bitwise and
+| // Bitwise or
+^ // Bitwise Xor
+~ // Bitwise one's complement
+<< // Bitwise shift left
+>> // Bitwise shift right
+
+
+Data Operators:
+sizeof() // Get the size of
+[] // Array subscript
+& // The address of
+* // The value of
+-> // Structure dereference
+. // Structure reference
+
+
