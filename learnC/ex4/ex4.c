e2ba48d853c2172da21f4d8ea30f381fc813b5c6
Masked Fox <iammaskedfx@gmail.com>
2020-10-20 18:33:51 -0400

    Completed ex 4, need to Watch the video though

---
+++ learnC/ex4/ex4.c
@@ -0,0 +1,18 @@
+//TODO Watch Video on Debugging
+
+gdb --args // i knew this one
+
+// TODO Need to watch the video to understand this one
+threat apply all bt // Dump a backtrace on all threads. It's very useful
+
+gdb --batch --ex r --ex bt --ex q --args //Run a program so that if it bombs you get a backtrace.
+
+info break
+
+info watchpoint //TODO Whats it for?
+
+attach pid
+
+detach
+
+
