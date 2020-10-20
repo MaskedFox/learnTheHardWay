8d0fceab997bf0a54857830154425ecf3bc7a315
Masked Fox <iammaskedfx@gmail.com>
2020-10-20 18:20:38 -0400

    Completed ex 3 and exercises

---
+++ learnC/ex3/ex3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 10;
+    int height = 72;
+
+    printf("Iam %d years old.\n", age );
+    printf("Iam %d inches tall.\n", height);
+
+    return 0;
+
+}
+
+//TODO Printf escape codes and format sequences exercise
+
+// Did all the break exercises
+
+// Extra Credit:
+// Broke the program as many ways as i could
+//TODO run "man 3 printf"
+
+
