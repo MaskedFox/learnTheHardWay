fed0887d7637f86efe0936a08ebedd843803eeb7
Masked Fox <iammaskedfx@gmail.com>
2020-12-10 18:31:26 -0500

    ex10-ex cred #2 Done

--- learnC/ex10/ex10.c
+++ learnC/ex10/ex10.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2)
+    if (argc < 2)
     {
         printf("ERROR: You need one argument.\n");
         // This is how you abort a program
@@ -11,50 +11,55 @@ int main(int argc, char *argv[])
     }
 
     int i = -1;
-    // argv[1] its the user argument for ex "Alex", thats 1 argument
-    //argv[1][0] its the user argument but on position 0, ex "A"
-    for ( ; i++, argv[1][i] != '\0';)
+    int wordCount = 1;
+    // argv[wordCount] its the user argument for ex "Alex", thats 1 argument
+    for (wordCount = 1; wordCount< argc; wordCount++)
     {
-        char letter, lowerLetter;
-        letter = argv[1][i];
-        lowerLetter = tolower(letter);
-
-        switch (lowerLetter)
+    //argv[wordCount][0] its the user argument but on position 0, ex "A"
+        for ( ; i++, argv[1][i] != '\0';)
         {
-            case 'a':
-                printf("%d: 'a'\n", i);
-                break;
+            char letter, lowerLetter;
+            letter = argv[1][i];
+            lowerLetter = tolower(letter);
 
-            case 'e':
-                printf("%d: 'e'\n",i);
-                break;
+            switch (lowerLetter)
+            {
+                case 'a':
+                    printf("%d: 'a'\n", i);
+                    break;
 
-            case 'i':
-                printf("%d: 'i'\n", i);
-                break;
+                case 'e':
+                    printf("%d: 'e'\n",i);
+                    break;
 
-            case 'o':
-                printf("%d: 'o'\n", i);
-                break;
+                case 'i':
+                    printf("%d: 'i'\n", i);
+                    break;
 
-            case 'u':
-                printf("%d: 'u'\n", i);
-                break;
+                case 'o':
+                    printf("%d: 'o'\n", i);
+                    break;
 
-            case 'y':
-                if (i > 2)
-                {
-                    // It's only sometiumes Y
-                    printf("%d: 'y'\n", i);
-                }
-                break;
+                case 'u':
+                    printf("%d: 'u'\n", i);
+                    break;
 
-            default:
-                printf("%d: %c is not a vowel\n", i, letter);
+                case 'y':
+                    if (i > 2)
+                    {
+                        // It's only sometiumes Y
+                        printf("%d: 'y'\n", i);
+                    }
+                    break;
+
+                default:
+                    printf("%d: %c is not a vowel\n", i, letter);
 
 
+            }
         }
     }
+
     return 0;
 }
 
