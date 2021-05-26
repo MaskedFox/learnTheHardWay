09fa697292145cf85e231acd4f441ae912eccbcd
Kali <iammaskedfx@gmail.com>
2021-05-25 20:23:09 -0400

    last well known code, before i start testing things

--- learnC/ex15/ex15.c
+++ learnC/ex15/ex15.c
@@ -24,7 +24,11 @@ int main(int argc, char *argv[])
 
     // set up the pointers to the start of the arrays
     int *cur_age = ages;
+    // creates a points of type char that points at names and
+    // at the same time it derefence it self? Am i right?
     char **cur_name = names;
+    printf("Printing **cur_name = names; %c\n", **cur_name);
+
 
     // second way using pointers
     for (i = 0; i < count; i++)
@@ -47,7 +51,7 @@ int main(int argc, char *argv[])
     for (cur_name = names, cur_age = ages;
         (cur_age - ages) < count; cur_name++, cur_age++)
         {
-            printf("%s lived %d years so far. \n", *cur_name, cur_age);
+            printf("%s lived %d years so far. \n", *cur_name, *cur_age);
         }
         
         return 0;
